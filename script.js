25a43aef03e0221de0afa5a7e0637b0b73d7a057
katya20 <kpanchenko21@gmail.com>
2016-09-28 08:42:55 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var colorfulElements = $(".intro-section")
 var colorNameDisplay = $("#color-display")
-console.log("hover");
+
 colorfulElements.hover(function(e) {
   var elementHoveredOver = $(e.target)
   colorNameDisplay.html("hi");
